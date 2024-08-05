6bcf5fae3e92327331a39e04ca2c0d3c15747ba1
Sergey <104456968+Solod-S@users.noreply.github.com>
2024-08-05 17:52:23 +0300

    add pagination logic, add sorting logic, add filtering logic
    add pagination logic, add sorting logic, add filtering logic

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,3 +1,28 @@
-const feed = (parent, args, context) => context.prisma.link.findMany();
+const feed = async (parent, args, context) => {
+  const where = args.filter
+    ? {
+        OR: [
+          { description: { contains: args.filter } },
+          { url: { contains: args.filter } },
+        ],
+      }
+    : {};
+
+  const links = await context.prisma.link.findMany({
+    where,
+    skip: args.skip,
+    take: args.take,
+    // пагинация
+    orderBy: args.orderBy,
+    // фильтрация
+  });
+
+  const count = await context.prisma.link.count({ where });
+
+  return {
+    links,
+    count,
+  };
+};
 
 module.exports = { feed };
